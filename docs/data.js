47ce368fcef8ba0b75dce063271aae8278f3667c
daich <fst-qst.w-it-h@docomo.ne.jp>
2019-12-29 06:14:30 +0000

    20191229 ADD synamicTable basic logic

---
+++ docs/data.js
@@ -0,0 +1,7 @@
+const instancesArray = {
+    myapl     : { "起動時刻":"2 days ago"    , "環境予約者":"daich", "DB向き先":"dev"      },
+    myweb     : { "起動時刻":"3 years ago"   , "環境予約者":"hiro" , "DB向き先":"prod"     },
+    myapl_dev1: { "起動時刻":"14 minutes ago", "環境予約者":"daich", "DB向き先":"dev"      },
+    myweb_dev : { "起動時刻":"9 days ago"    , "環境予約者":"chika", "DB向き先":"content"  },
+    myapl_dev2: { "起動時刻":"just before"   , "環境予約者":"yoko" , "DB向き先":"dev"      }
+}
